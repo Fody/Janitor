ef2f2bbdf0f060907218fbca6f2cca1f99cd8c56
Simon Cropp <simon.cropp@gmail.com>
2013-09-30 17:02:45 +1000

    bump refs

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("Janitor")]
-[assembly: AssemblyProduct("Janitor")]
 [assembly: AssemblyVersion("0.3.0")]
 [assembly: AssemblyFileVersion("0.3.0")]
