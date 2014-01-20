0016b07be0a2985c37bbcbafd3c844f4e058ce87
Cameron MacFarland <distantcam@gmail.com>
2014-01-20 11:07:25 +0800

    Better logging

--- Fody/ModuleWeaver.cs
+++ Fody/ModuleWeaver.cs
@@ -1,27 +1,34 @@
 ﻿using System;
 using System.Linq;
 using Mono.Cecil;
-using Mono.Cecil.Cil;
 
 public partial class ModuleWeaver
 {
     public Action<string> LogInfo { get; set; }
+    public Action<string> LogWarning { get; set; }
+    public Action<string> LogError { get; set; }
     public ModuleDefinition ModuleDefinition { get; set; }
     public IAssemblyResolver AssemblyResolver { get; set; }
 
     public ModuleWeaver()
     {
         LogInfo = s => { };
+        LogWarning = s => { };
+        LogError = s => { };
     }
 
     public void Execute()
     {
+        LoggerFactory.LogInfo = LogInfo;
+        LoggerFactory.LogWarn = LogWarning;
+        LoggerFactory.LogError = LogError;
+
         FindCoreReferences();
 
         foreach (var type in ModuleDefinition
             .GetTypes()
-            .Where(x => 
-                x.IsClass() && 
+            .Where(x =>
+                x.IsClass() &&
                 !x.IsAbstract &&
                 !x.IsGeneratedCode() &&
                 !x.CustomAttributes.ContainsSkipWeaving()))
@@ -36,19 +43,19 @@ public partial class ModuleWeaver
             if (disposeMethods.Count > 1)
             {
                 var message = string.Format("Type `{0}` contains more than one `Dispose` method. Either remove one or add a `[Janitor.SkipWeaving]` attribute to the type.", type.FullName);
-                throw new WeavingException(message);
+                LogError(message);
             }
             var disposeMethod = disposeMethods.First();
 
             if (!disposeMethod.IsEmptyOrNotImplemented())
             {
                 var message = string.Format("Type `{0}` contains a `Dispose` method with code. Either remove the code or add a `[Janitor.SkipWeaving]` attribute to the type.", type.FullName);
-                throw new WeavingException(message);
+                LogError(message);
             }
             if (type.BaseType.Name != "Object")
             {
                 var message = string.Format("Type `{0}` has a base class which is not currently supported. Either remove the base class or add a `[Janitor.SkipWeaving]` attribute to the type.", type.FullName);
-                throw new WeavingException(message);
+                LogError(message);
             }
 
             var methodProcessor = new TypeProcessor
