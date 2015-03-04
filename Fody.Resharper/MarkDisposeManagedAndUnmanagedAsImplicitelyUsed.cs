27baa73228a3d584e7266b8a89ea63c39c27973e
danielmarbach <daniel.marbach@openplace.net>
2015-03-04 22:11:58 +0100

    Draft of Resharper plugin

---
+++ Fody.Resharper/MarkDisposeManagedAndUnmanagedAsImplicitelyUsed.cs
@@ -0,0 +1,26 @@
+﻿namespace Janitor.Fody.Resharper
+{
+    using JetBrains.Annotations;
+    using JetBrains.Application;
+    using JetBrains.ReSharper.Daemon.UsageChecking;
+    using JetBrains.ReSharper.Psi;
+
+    [ShellComponent]
+    public class MarkDisposeManagedAndUnmanagedAsImplicitelyUsed : IUsageInspectionsSuppressor
+    {
+        public bool SuppressUsageInspectionsOnElement(IDeclaredElement element, out ImplicitUseKindFlags flags)
+        {
+            flags = 0;
+            var method = element as IMethod;
+            if (method == null)
+                return false;
+            var containingType = method.GetContainingType() as IClass;
+            if (containingType == null)
+                return false;
+            if ((method.ShortName != "DisposeManaged" && method.ShortName != "DisposeUnmanaged") || method.IsStatic)
+                return false;
+            flags = ImplicitUseKindFlags.Default;
+            return true;
+        }
+    }
+}
