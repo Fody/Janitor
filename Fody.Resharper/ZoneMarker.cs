c1386db305ad06daf409bf0d60835417a0e16fc2
danielmarbach <daniel.marbach@openplace.net>
2015-03-05 22:17:44 +0100

    Added necessary zone marker.

---
+++ Fody.Resharper/ZoneMarker.cs
@@ -0,0 +1,10 @@
+﻿namespace Janitor.Fody.Resharper
+{
+    using JetBrains.Application.BuildScript.Application.Zones;
+    using JetBrains.ReSharper.Psi;
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<IClrPsiLanguageZone>
+    {
+    }
+}
