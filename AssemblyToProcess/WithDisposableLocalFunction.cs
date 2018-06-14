926c308787fb692c3fc36d6cf09b5d4fc3a54e66
Jason Woods <jwoods@appliedsystems.com>
2018-06-14 00:37:10 -0500

    nested generated types not being skipped (#29)

---
+++ AssemblyToProcess/WithDisposableLocalFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class WithDisposableLocalFunction
+{
+    public IEnumerable<string> MethodWithLocalFunction()
+    {
+        return ContainedTypes(typeof(IDictionary<int, string>)).ToList();
+
+        IEnumerable<string> ContainedTypes(Type type)
+        {
+            yield return type.FullName;
+
+            if (type.IsGenericType)
+            {
+                foreach (var containedType in
+                    type.GenericTypeArguments.SelectMany(ContainedTypes))
+                {
+                    yield return containedType;
+                }
+            }
+        }
+    }
+}
